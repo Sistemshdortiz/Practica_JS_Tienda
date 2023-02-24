e24e97d185deba77e16c65cad7cf54f4a9543f8e
Sistemshdortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2023-02-24 19:33:11 +0100

    localstorage funcional

--- ajax.js
+++ ajax.js
@@ -1,63 +1,58 @@
+
+
 $(document).ready(function () {
  
+    let almacen = JSON.parse(localStorage.getItem('almacen')) || [];
+
     $('#boton1').click(function () {
 
-        alert("!!!")
         const producto = {
             codigo: null,
             descripcion: null,
             importe: null,
             stock: null
-        };
-
+        };     
+        
         producto.codigo = $('input:text[id=cod_producto]').val()
         producto.descripcion = $('input:text[id=descrip]').val()
         producto.importe = $('input:text[id=importe]').val()
         producto.stock = $('input:text[id=stock]').val()
-        // Convertir objeto a cadena JSON
-        const productoJSON = JSON.stringify(producto);
+
+        almacen.push(producto)
+        
+        const productoJSON = JSON.stringify(almacen);
 
         // Guardar cadena JSON en localStorage
-        localStorage.setItem('producto', productoJSON);
+      
+        localStorage.setItem('almacen', productoJSON);
         
         alert($('input:text[id=descrip]').val())
         alert($('input:text[id=importe]').val())
         alert($('input:text[id=stock]').val())
     
+        // const producto2= JSON.parse(localStorage.getItem('almacen2'))
+        // console.log(producto2);
         
     });
 
     $('#boton2').click(function () {
         alert("segundo btn.")
         // Obtener cadena JSON del almacenamiento local
-        const productoJSON = localStorage.getItem('producto');
-
-        //obtener datos del porducto para pasarlo al xml
-        // let data = localStorage.getItem('producto')
-        // let xhr = new XMLHttpRequest()
-        // let url = 'productos.xml'
-        // xhr.open('POST', url, true);
-        // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-        // let params = 'producto='+encodeURIComponent(data);
-        // xhr.send(params);
-
-        // xhr.onreadystatechange = function(){
-        //     if(xhr.readyState ===4 && xhr.status ===200){
-        //         console.log(xhr.responseText)
-        //     }
-        // }
+        const almacenJSON = localStorage.getItem('almacen');
 
         // Convertir cadena JSON a objeto
-        const producto = JSON.parse(productoJSON);
+        const producto = JSON.parse(almacenJSON);
     
         // Acceder a las propiedades del objeto
-        console.log(producto.codigo);        
-        console.log(producto.descripcion);  
-        console.log(producto.importe);      
-        console.log(producto.stock);   
-        
+        for(i=0; i<producto.length; i++){
+            console.log(producto[i]); 
+        }
+            
+        // console.log(producto.descripcion);  
+        // console.log(producto.importe);      
+        // console.log(producto.stock);   
 
-    
+  
     });
     
 });
