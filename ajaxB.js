04758e8eeedb7254538a0e093812e7fd8dabee47
Kevjoeam <kjammirata@gmail.com>
2023-02-24 21:34:09 +0100

    actualizadoTODO

--- ajaxB.js
+++ ajaxB.js
@@ -1,61 +1,57 @@
-document.addEventListener('onload', function() {
-const buscarInput = document.querySelector('#busqueda');
-const listaSugerencias = document.querySelector('#sugerencia');
+$(document).ready(function () {
+  const buscarInput = document.querySelector('#busqueda');
+  const listaSugerencias = document.querySelector('#sugerencia');
 
-buscarInput.addEventListener('input', busqueda);
+  buscarInput.addEventListener('input', busqueda);
 
-function busqueda() {
+  function busqueda() {
     const textoBuscado = buscarInput.value;
     if (textoBuscado.trim().length > 0) {
-        getSugerencias(textoBuscado)
-            .then(mostrarSugerencias)
-            .catch(gestionarError);
+      getSugerencias(textoBuscado)
+        .then(mostrarSugerencias)
+        .catch(gestionarError);
     } else {
-        limpiarSugerencias();
+      limpiarSugerencias();
     }
-}
+  }
 
-function getSugerencias(textoBuscado) {
-  return new Promise((resolve, reject) => {
-      $.ajax({
-        url: 'productos.xml',
-        dataType: 'xml',
-        success: function(data) {
-          const sugerencias = [];
-          $(data).find('producto').each(function() {
-            const descripcion = $(this).find('descripcion').text();
-            sugerencias.push(
-            
-              descripcion
-            );
-          });
-          console.log(sugerencias)
-          const sugerenciasFiltradas = sugerencias.filter(sugerencia =>
-            sugerencia.descripcion.toLowerCase().startsWith(textoBuscado.toLowerCase())
-          );
-          resolve(sugerenciasFiltradas);
-        },
-        error: function() {
-          reject('Ha ocurrido un error al obtener las sugerencias.');
-        }
-      });
+  function getSugerencias(textoBuscado) {
+    return new Promise((resolve, reject) => {
+      const datos = localStorage.getItem('almacen');
+      const data = JSON.parse(datos);
+      console.log(data)
+      if (!data) {
+        reject('No se encontraron datos en Local Storage.');
+      }
+      const sugerencias = [];
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i])
+        const descripcion = data[i].descripcion;
+        sugerencias.push(descripcion);
+      }
+      const sugerenciasFiltradas = sugerencias.filter(sugerencia =>
+        sugerencia.toLowerCase().startsWith(textoBuscado.toLowerCase()));
+      resolve(sugerenciasFiltradas);
+      console.log(sugerenciasFiltradas)
+
     });
-}
 
-function mostrarSugerencias(sugerencias) {
+  }
+
+  function mostrarSugerencias(sugerencias) {
     listaSugerencias.innerHTML = '';
     sugerencias.forEach(sugerencia => {
-        const li = document.createElement('li');
-        li.textContent = sugerencia;
-        listaSugerencias.appendChild(li);
+      const li = document.createElement('li');
+      li.textContent = sugerencia;
+      listaSugerencias.appendChild(li);
     });
-}
+  }
 
-function gestionarError(error) {
+  function gestionarError(error) {
     console.error(error);
-}
+  }
 
-function limpiarSugerencias() {
+  function limpiarSugerencias() {
     listaSugerencias.innerHTML = '';
-}
-})
+  }
+});
